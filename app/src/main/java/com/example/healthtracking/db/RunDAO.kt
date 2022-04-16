70f80aa11892788d472a3794bf7360246e235c1b
rustam <r.kozhinov@mail.ru>
2022-04-16 20:29:18 +1000

    add dao class db

---
+++ app/src/main/java/com/example/healthtracking/db/RunDAO.kt
@@ -0,0 +1,52 @@
+package com.example.healthtracking.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface RunDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRun(run: Run)
+
+    @Delete
+    suspend fun deleteRun(run: Run)
+
+    //get all runs sorted by date
+    @Query("SELECT * FROM running_table ORDER BY timestamp DESC")
+    fun getAllRunsSortedByDate(): LiveData<List<Run>>
+
+    //get all runs sorted by time in mills
+    @Query("SELECT * FROM running_table ORDER BY timeInMillis DESC")
+    fun getAllRunsSortedByTimeInMillis(): LiveData<List<Run>>
+
+    //get all runs sorted by calories burned
+    @Query("SELECT * FROM running_table ORDER BY caloriesBurned DESC")
+    fun getAllRunsSortedByCaloriesBurned(): LiveData<List<Run>>
+
+    //get all runs sorted by distance
+    @Query("SELECT * FROM running_table ORDER BY distanceInMeters DESC")
+    fun getAllRunsSortedByDistance(): LiveData<List<Run>>
+
+    //get all runs sorted by avg speed
+    @Query("SELECT * FROM running_table ORDER BY avgSpeedInKMH DESC")
+    fun getAllRunsSortedByAvgSpeed(): LiveData<List<Run>>
+
+    //get total time in millis
+    @Query("SELECT SUM(timeInMillis) FROM running_table")
+    fun getTotalTimeInMillis(): LiveData<Long>
+
+    //get total distance
+    @Query("SELECT SUM(distanceInMeters) FROM running_table")
+    fun getTotalDistance(): LiveData<Int>
+
+    //get total avg speed
+    @Query("SELECT AVG(avgSpeedInKMH) FROM running_table")
+    fun getTotalAvgSpeed(): LiveData<Float>
+
+    //get total calories burned
+    @Query("SELECT SUM(caloriesBurned) FROM running_table")
+    fun getTotalCaloriesBurned(): LiveData<Long>
+
+
+}
