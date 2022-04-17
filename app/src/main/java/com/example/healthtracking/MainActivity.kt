b2048e8429bf08ae03010d444ef21a72f09d8737
rustam <r.kozhinov@mail.ru>
2022-04-17 16:08:37 +1000

    dagger-hilt setup

--- app/src/main/java/com/example/healthtracking/MainActivity.kt
+++ app/src/main/java/com/example/healthtracking/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.healthtracking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+import com.example.healthtracking.db.RunDao
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var runDao: RunDao
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("runDao", "RUNDAO: ${runDao.hashCode()}")
     }
 }
