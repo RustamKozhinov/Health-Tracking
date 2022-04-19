713590d458c48fdaf5d1d1962a5dc6c040fa7a06
rustam <r.kozhinov@mail.ru>
2022-04-19 18:48:45 +1000

    tracking service basic setup

--- app/src/main/java/com/example/healthtracking/other/Constants.kt
+++ app/src/main/java/com/example/healthtracking/other/Constants.kt
@@ -1,7 +1,15 @@
 package com.example.healthtracking.other
 
 object Constants {
+
+    //database constant
     const val RUNNING_DATABASE_NAME = "running_db"
 
+    //location permission constant
     const val REQUEST_CODE_LOCATION_PERMISSION = 0
+
+    //tracking service constants
+    const val ACTION_START_OR_RESUME_SERVICE = "ACTION_START_OR_RESUME_SERVICE"
+    const val ACTION_PAUSE_SERVICE = "ACTION_PAUSE_SERVICE"
+    const val ACTION_STOP_SERVICE = "ACTION_STOP_SERVICE"
 }
--- app/src/main/java/com/example/healthtracking/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/example/healthtracking/ui/fragments/TrackingFragment.kt
@@ -1,15 +1,18 @@
 package com.example.healthtracking.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import com.example.healthtracking.services.TrackingService
 import com.example.healthtracking.R
 import com.example.healthtracking.viewmodels.MainViewModel
 import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
+import com.example.healthtracking.other.Constants.ACTION_START_OR_RESUME_SERVICE
 import kotlinx.android.synthetic.main.fragment_tracking.*
 
 /*
@@ -28,8 +31,15 @@ class TrackingFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        //button a timer
+        btnToggleRun.setOnClickListener {
+            sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+        }
+
         mapView.onCreate(savedInstanceState)
 
+        //setup map
         mapView.getMapAsync {
             map = it
         }
@@ -37,26 +47,38 @@ class TrackingFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_tracking, container, false)
     }
 
+    //when you press the button, the timer runs in the background
+    private fun sendCommandToService(action: String) =
+        Intent(requireContext(), TrackingService::class.java).also {
+            it.action = action
+            requireContext().startService(it)
+        }
+
+    //the map's lifecycle
     override fun onResume() {
         super.onResume()
         mapView?.onResume()
     }
 
+    //the map's lifecycle
     override fun onStart() {
         super.onStart()
         mapView?.onStart()
     }
 
+    //the map's lifecycle
     override fun onStop() {
         super.onStop()
         mapView?.onStop()
     }
 
+    //the map's lifecycle
     override fun onPause() {
         super.onPause()
         mapView?.onPause()
     }
 
+    //the map's lifecycle
     override fun onLowMemory() {
         super.onLowMemory()
         mapView?.onLowMemory()
