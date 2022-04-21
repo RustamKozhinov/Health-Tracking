c197d056864fe273b4cd05242d67e7992c2e935b
rustam <r.kozhinov@mail.ru>
2022-04-21 10:11:44 +1000

    cancel tracking

--- app/src/main/java/com/example/healthtracking/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/example/healthtracking/ui/fragments/TrackingFragment.kt
@@ -2,12 +2,11 @@ package com.example.healthtracking.ui.fragments
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.example.healthtracking.services.TrackingService
 import com.example.healthtracking.R
 import com.example.healthtracking.viewmodels.MainViewModel
@@ -16,6 +15,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import com.example.healthtracking.other.Constants.ACTION_START_OR_RESUME_SERVICE
 import com.example.healthtracking.other.Constants.POLYLINE_COLOR
 import com.example.healthtracking.other.Constants.POLYLINE_WIDTH
+import com.example.healthtracking.other.Constants.ACTION_STOP_SERVICE
 import com.example.healthtracking.other.Constants.MAP_ZOOM
 import com.example.healthtracking.other.Constants.ACTION_PAUSE_SERVICE
 import com.example.healthtracking.other.TrackingUtility
@@ -24,6 +24,7 @@ import com.example.healthtracking.services.Polyline
 
 
 import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.android.synthetic.main.fragment_tracking.*
 
 
@@ -46,27 +47,30 @@ class TrackingFragment : Fragment() {
     //stop watch
     private var curTimeInMillis = 0L
 
+    private var menu: Menu? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
+        return inflater.inflate(R.layout.fragment_tracking, container, false)
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView.onCreate(savedInstanceState)
         //button a timer
         btnToggleRun.setOnClickListener {
             toggleRun()
         }
-
-        //the map's lifecycle
-        mapView.onCreate(savedInstanceState)
-
         //setup map
         mapView.getMapAsync {
             map = it
             addAllPolylines()
         }
-        subscribeToObservers()
 
-        return inflater.inflate(R.layout.fragment_tracking, container, false)
+        subscribeToObservers()
     }
 
     /*
@@ -98,6 +102,7 @@ class TrackingFragment : Fragment() {
     */
     private fun toggleRun() {
         if(isTracking) {
+            menu?.getItem(0)?.isVisible = true
             sendCommandToService(ACTION_PAUSE_SERVICE)
         } else {
             sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
@@ -115,6 +120,7 @@ class TrackingFragment : Fragment() {
             btnFinishRun.visibility = View.VISIBLE
         } else {
             btnToggleRun.text = "Stop"
+            menu?.getItem(0)?.isVisible = true
             btnFinishRun.visibility = View.GONE
         }
     }
@@ -171,6 +177,50 @@ class TrackingFragment : Fragment() {
             it.action = action
             requireContext().startService(it)
         }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.toolbar_tracking_menu, menu)
+        this.menu = menu
+    }
+
+    //change menu item visibility
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        super.onPrepareOptionsMenu(menu)
+        if(curTimeInMillis > 0L) {
+            this.menu?.getItem(0)?.isVisible = true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.miCancelTracking -> {
+                showCancelTrackingDialog()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    //show cancel tracking dialog
+    private fun showCancelTrackingDialog() {
+        val dialog = MaterialAlertDialogBuilder(requireContext(), R.style.AlertDialogTheme)
+            .setTitle("Cancel the Run?")
+            .setMessage("Are you sure to cancel the current run and delete all its data?")
+            .setIcon(R.drawable.ic_delete)
+            .setPositiveButton("Yes") { _, _ ->
+                stopRun()
+            }
+            .setNegativeButton("No") { dialogInterface, _ ->
+                dialogInterface.cancel()
+            }
+            .create()
+        dialog.show()
+    }
+
+    private fun stopRun() {
+        sendCommandToService(ACTION_STOP_SERVICE)
+        findNavController().navigate(R.id.action_trackingFragment_to_runFragment)
+    }
 
     //the map's lifecycle
     override fun onResume() {
